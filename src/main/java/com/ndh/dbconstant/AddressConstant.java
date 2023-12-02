640c66f3c6141cd4b039fed34f85d7244e30ddbd
DucPham1306 <phuduc1306@gmail.com>
2023-12-02 12:57:09 +0700

    test

--- src/main/java/com/ndh/dbconstant/AddressConstant.java
+++ src/main/java/com/ndh/dbconstant/AddressConstant.java
@@ -4,4 +4,6 @@ public class AddressConstant {
     public static final String I_ID         = "I_ID";
     public static final String I_ID_USER    = "I_ID_USER";
     public static final String T_ADDRESS    = "T_ADDRESS";
+
+    public static final String rawewa = "";
 }
